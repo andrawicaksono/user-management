b52e9aef00fce9e336c485a95f2eb2dfd25c34bd
Andra Wicaksono <andrawicaksono14@gmail.com>
2023-11-02 16:24:49 +0700

    change export method

--- src/api/v1/controllers/check.js
+++ src/api/v1/controllers/check.js
@@ -1,11 +1,13 @@
-module.exports.CheckController = {
-    check: (req, res, next) => {
-        try {
-            return res.status(200).send({
-                message: "OK",
-            });
-        } catch (err) {
-            next(err);
+module.exports.CheckController = () => {
+    return {
+        check: (req, res, next) => {
+            try {
+                return res.status(200).send({
+                    message: "OK",
+                });
+            } catch (err) {
+                next(err);
+            }
         }
-    },
+    }
 };
