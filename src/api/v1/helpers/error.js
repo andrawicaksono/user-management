d0872b61ccf1953ce05973439e45d845b5bcb3cc
Andra Wicaksono <andrawicaksono14@gmail.com>
2023-11-02 17:09:51 +0700

    change error code for access denied

--- src/api/v1/helpers/error.js
+++ src/api/v1/helpers/error.js
@@ -2,5 +2,5 @@ module.exports.ERROR = {
     EMAIL_REGISTERED: Error("error;400;001;email has already registered!"),
     USER_NOT_FOUND: Error("error;404;002;user not found!"),
     WRONG_PASSWORD: Error("error;400;003;wrong password!"),
-    ACCESS_DENIED: Error("error;400;004;access denied!")
+    ACCESS_DENIED: Error("error;403;004;access denied!")
 } 
